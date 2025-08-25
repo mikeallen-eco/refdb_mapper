b9fb607d71e348c2f924e7e1e5b16ba5bf3b8337
Mike Allen <32096773+mikeallen-eco@users.noreply.github.com>
2025-08-25 16:34:01 -0400

    pipelined through: curate amplicons

--- pipeline/R/settings_Vences_16S.R
+++
@@ -1,7 +0,0 @@
-out_path <- "~/Documents/mikedata/refdb_geo/mammals_Vences_16S/"
-db_name <- "V16S_mammalia_midori265_tax20250609"
-l <- 100; L <- 300
-fwd <- "AGACGAGAAGACCCYdTGGAGCTT"; rev <- "GATCCAACATCGAGGTCGTAA"
-raw_refdb_path <- "~/Documents/mikedata/refdb_geo/MIDORI2_UNIQ_NUC_GB265_lrRNA_RDP.fasta"
-marker <- "Vences_16S"
-LOO_refdb_path <- paste0(out_path, "refdb_", db_name, ".fasta")
---
+++ pipeline/initialize.sh
@@ -0,0 +1,5 @@
+# setup_pipeline.sh
+
+conda env create -f environment.yaml
+
+Rscript ~/Documents/research/edna_pipeline/blastg/install.R
